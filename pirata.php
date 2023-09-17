255db44f4e077a01ee551f08f0e3e6d75cc34ee2
Calistenico <99051935+Calistenico@users.noreply.github.com>
2023-09-17 09:47:50 -0300

    Update and rename pirata.html to pirata.php

---
+++ pirata.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    
+    // Caminho para o arquivo de texto onde os emails serão armazenados
+    $file = "C:\\Users\\Teste 2022\\Desktop\\site\\emails.txt";
+    
+    // Abre o arquivo em modo de escrita (append)
+    $fileHandle = fopen($file, "a");
+    
+    // Verifica se o arquivo foi aberto com sucesso
+    if ($fileHandle) {
+        // Formata a linha que será escrita no arquivo
+        $data = "Nome: " . $name . "\nE-mail: " . $email . "\n\n";
+        
+        // Escreve a linha no arquivo
+        fwrite($fileHandle, $data);
+        
+        // Fecha o arquivo
+        fclose($fileHandle);
+        
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Ocorreu um erro ao cadastrar. Por favor, tente novamente mais tarde.";
+    }
+} else {
+    echo "Acesso inválido ao script.";
+}
+?>
