42c9ce56081dda7c54ddd93927884f1c57ac4725
Calistenico <99051935+Calistenico@users.noreply.github.com>
2023-09-17 17:46:49 -0300

    Update pirata.php

--- pirata.php
+++ pirata.php
@@ -1,31 +1,152 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    
-    // Caminho para o arquivo de texto onde os emails serão armazenados
-    $file = "cadastros/dados.txt";
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title>Curso de Gestão de Tráfego</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-image: url('https://img-c.udemycdn.com/course/750x422/3941728_660d_6.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        @media screen and (max-width: 600px) {
+            /* Estilos para telas menores que 600px */
+        }
+        
+        .overlay {
+            background-color: rgba(0, 0, 0, 0.418);
+        }
+        .header {
+            background-color: #3333338e;
+            color: #fff;
+            text-align: center;
+            padding: 2rem;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem;
+            background-color: rgba(75, 59, 59, 0.603);
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.034);
+        }
+        .headline {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+        .copy {
+            font-size: 21px;
+            line-height: 1.6;
+        }
+        .cta-button {
+            display: inline-block;
+            padding: 15px 30px;
+            background-color: #ff5733;
+            color: #fff;
+            font-size: 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+        .cta-button:hover {
+            background-color: #e64422;
+        }
+        /* Emojis e espaço para fotos */
+        .emojis {
+            text-align: center;
+            font-size: 30px;
+        }
+        .photos {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+        .photo {
+            width: 100px;
+            height: 100px;
+            margin: 0 10px;
+            border-radius: 50%;
+        }
+        /* Informações de contato */
+        .contact {
+            text-align: center;
+            margin-top: 20px;
+        }
+        .course-price {
+            background-color: #ff5733; /* Cor de fundo do preço */
+            color: white; /* Cor do texto do preço */
+            font-size: 18px;
+            padding: 5px 10px;
+            border-radius: 5px;
+            margin-left: 10px; /* Espaço à esquerda do preço */
+            display: inline-block; /* Para alinhar ao lado do botão */
+        }
+        
+        .contact a {
+            color: #ff5733;
+            text-decoration: none;
+            font-weight: bold;
+            margin: 0 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="overlay"></div>
+    <div class="header">
+        <h1 style="font-family: 'Arial', sans-serif; font-size: 36px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">Curso de Gestão de Tráfego</h1>
+        <p style="font-family: 'Arial', sans-serif; font-size: 20px;">Aprenda a dominar o marketing digital e conquiste resultados incríveis! 🚀</p>
+    </div>
     
-    // Abre o arquivo em modo de escrita (append)
-    $fileHandle = fopen($file, "a");
-    
-    // Verifica se o arquivo foi aberto com sucesso
-    if ($fileHandle) {
-        // Formata a linha que será escrita no arquivo
-        $data = "Nome: " . $name . "\nE-mail: " . $email . "\n\n";
+    <div class="photos">
+        <img class="photo" src="https://thundermustard.com/wp-content/uploads/2021/07/Gestao-de-Trafego-O-que-e-e-como-funciona.png" alt="Foto 1">
+        <img class="photo" src="https://taumkt.com.br/wp-content/uploads/2022/03/Veja-todos-os-Benefi%CC%81cios-da-Gesta%CC%83o-de-Tra%CC%81fego.jpg" alt="Foto 2">
+        <img class="photo" src="https://www.agenciaclara.com.br/uploads/images/2020/10/como-e-feita-a-gestao-de-trafego-1602533839.webp" alt="Foto 3">
+        <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY_BaRWySsxkdhpDkGjVWCpsRSmm6AFIyXkQ&usqp=CAU" alt="Foto 1">
+        <img class="photo" src="https://www.designi.com.br/images/preview/10654820.jpg" alt="Foto 1">
+    </div>
+    <div class="container">
+        <div class="headline">Por que você deve se inscrever no nosso curso?</div>
+        <div class="copy" style="color: white;">
+            <p><strong>Aprenda com os melhores 🌟:</strong> Nossos especialistas em marketing digital têm um histórico comprovado de sucesso em gestão de tráfego. 🚀</p>
+            <p><strong>Conhecimento prático e eficaz 📈:</strong> Este curso oferece orientação prática para transformar cliques em conversões e clientes leais. Você verá resultados imediatos! 💡</p>
+            <p><strong>Domine as principais plataformas 📱:</strong> Descubra os segredos por trás do tráfego pago no Google Ads, Facebook, Instagram e muito mais. Esteja à frente da concorrência! 🏆</p>
+            <p><strong>Aumente sua receita 💰:</strong> Saiba como otimizar suas campanhas para obter o máximo retorno sobre o investimento. Cada clique conta! 💸</p>
+            <p><strong>Bônus exclusivos para os mais rápidos 🎁:</strong> Inscrições antecipadas recebem uma sessão de coaching individual para impulsionar seu sucesso. Além disso, tenha acesso vitalício ao material do curso e junte-se à nossa comunidade exclusiva de alunos. Não perca essa oportunidade! 🚀</p>
+        </div>
         
-        // Escreve a linha no arquivo
-        fwrite($fileHandle, $data);
+        <div class="cta-button">
+            <a href="https://go.hotmart.com/D86832944Q">Inscreva-se agora</a>
+            <div class="course-price">
+               Apenas: R$ 24,99
+            </div>
+        </div>
         
-        // Fecha o arquivo
-        fclose($fileHandle);
-        
-        echo "Cadastro realizado com sucesso!";
-    } else {
-        echo "Ocorreu um erro ao cadastrar. Por favor, tente novamente mais tarde.";
-    }
-} else {
-    echo "Acesso inválido ao script.";
-}
-?>
+        <!-- Emojis e espaço para fotos -->
+        <div class="emojis">
+            😃✨📈
+        </div>
+            <!-- Local para quatro fotos -->
+        <div class="photo-container">
+          <img class="photo" src="https://cookieweb.com.br/wp-content/uploads/2023/06/melhor-agencia-de-trafego-pago.png" alt="Foto 1">
+          <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTySK_JPyQmDnUzpC2xXeMwgIUCpgtZKQlyfA&usqp=CAU" alt="Foto 2">
+          <img class="photo" src="https://teletype.in/files/16/39/16398302-bd70-4340-b6ea-4a4790e34e07.png" alt="Foto 3">
+          <img class="photo" src="https://gomadigital.com.br/wp-content/uploads/2022/04/Gestao-de-trafego-para-pequenas-empresas-1.png" alt="Foto 4">
+          <img class="photo" src="https://i.ytimg.com/vi/JPa2dhF3zFg/maxresdefault.jpg" alt="Foto 1">
+          <img class="photo" src="https://rodlopes.com.br/wp-content/uploads/2022/09/Como-Comecar-Na-Gestao-De-Trafego-Pago.jpg" alt="Foto 1">
+        </div>
+          <!-- Informações de contato -->
+        <div class="contact">
+            Fale conosco: 
+            <a href="https://wa.me/48999626351" target="_blank">WhatsApp</a>
+            <a href="https://www.instagram.com/o_tal_do_pirata_ofc" target="_blank">Instagram</a>
+        </div>
+    </div>
+</body>
+</html>
